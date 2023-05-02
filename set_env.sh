5728aeb8b1ae513e2229f12aba07ab98fe16a760
mitchey <michael.uzor@gmail.com>
2023-05-02 22:11:46 +0100

    deployment files updated

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=mitchey
+export POSTGRES_PASSWORD=micha3!manch3st3r
+export POSTGRES_HOST=mitchey-db-dev.caumjphjqkkr.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::mitchey-microservices-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
